4eb8e8dd70e2ef198e53104b771b1cde3e7fd150
enoquerogerio <enoquerogerio15mti25@gmail.com>
2025-03-14 02:26:54 +0100

    ui design completed

---
+++ frontend/src/components/CreateUserModal.jsx
@@ -0,0 +1,72 @@
+import {
+  Button,
+  Modal,
+  useDisclosure,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalCloseButton,
+  ModalBody,
+  Flex,
+  FormControl,
+  FormLabel,
+  Input,
+  Textarea,
+  RadioGroup,
+  Radio,
+  ModalFooter,
+} from "@chakra-ui/react";
+import { BiAddToQueue } from "react-icons/bi";
+
+const CreateUserModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <>
+      <Button onClick={onOpen}>
+        <BiAddToQueue size={20} />
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>My new Nigga 👊</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Flex alignItems={"center"} gap={4}>
+              {/* Left */}
+              <FormControl>
+                <FormLabel>Full Name</FormLabel>
+                <Input placeholder="John Doe" />
+              </FormControl>
+              {/* Right */}
+              <FormControl>
+                <FormLabel>Role</FormLabel>
+                <Input placeholder="Software Engineer" />
+              </FormControl>
+            </Flex>
+            <FormControl mt={4}>
+              <FormLabel>Description</FormLabel>
+              <Textarea
+                resize={"none"}
+                overflow={"hidden"}
+                placeholder="He's a software engineer who loves to code and build things."
+              />
+            </FormControl>
+            <RadioGroup mt={4}>
+              <Flex gap={5}>
+                <Radio value="male">Male</Radio>
+                <Radio value="female">Female</Radio>
+              </Flex>
+            </RadioGroup>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Add
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+export default CreateUserModal;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,43 @@
+import { Box, Button, Container, Flex, Text } from "@chakra-ui/react";
+import { IoMoon } from "react-icons/io5";
+import { LuSun } from "react-icons/lu";
+import { useColorMode, useColorModeValue } from "@chakra-ui/react";
+import CreateUserModal from "./CreateUserModal";
+const Navbar = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Container maxW={"900px"}>
+      <Box
+        px={4}
+        my={4}
+        borderRadius={5}
+        bg={useColorModeValue("gray.200", "gray.700")}
+      >
+        <Flex h="16" alignItems={"center"} justifyContent={"space-between"}>
+          <Flex
+            alignItems={"center"}
+            justifyContent={"center"}
+            gap={3}
+            display={{ base: "none", sm: "flex" }}
+          >
+            <img src="/image.png" alt="Explode head" width={45} height={45} />
+          </Flex>
+          <Flex gap={3} alignItems={"center"}>
+            <Text
+              fontSize={"lg"}
+              fontWeight={500}
+              display={{ base: "none", md: "block" }}
+            >
+              STAFFHUB 🔥
+            </Text>
+            <Button onClick={toggleColorMode}>
+              {colorMode === "light" ? <IoMoon /> : <LuSun size={20} />}
+            </Button>
+            <CreateUserModal />
+          </Flex>
+        </Flex>
+      </Box>
+    </Container>
+  );
+};
+export default Navbar;
